4d8fa9991b0fd2401189717db5c232e12e7244f1
Ivan <clicker.heroes.acg@gmail.com>
2023-02-24 03:24:56 +0300

    test theorem

--- src/theorems/main.v
+++ src/theorems/main.v
@@ -1,11 +1,27 @@
+(* Здесь я описываю что из себя представляет bool  *)
 Inductive bool := 
 | true
 | false.
-
+(*
+ Объявления определения, насколько я понял
+ это просто аналог функции
+ эта обычная функция отрицания, то есть если мы
+ вызовем ее на false результатом получим true
+*)
 Definition negb (b : bool) : bool :=
    match b with
    | true => false
    | false => true
    end.
 
+(* так выглядит вызов ф-ции *)
 Eval compute in negb false.
+(*Здесь самое интересное*)
+Theorem negb_negb: forall (b : bool), negb (negb b) = b.
+Proof.
+   intros b.
+   (* Здесь наше выражение до упрощения*)
+   destruct b.
+   + simpl. reflexivity.
+   + simpl. reflexivity.
+Qed.
